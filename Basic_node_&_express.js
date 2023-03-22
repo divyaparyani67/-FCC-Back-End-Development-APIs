519eab67f5173d2c29d2a5eaa5654f89653fb78d
Divya Paryani <106226581+divyaparyani67@users.noreply.github.com>
2023-03-22 23:45:49 +0530

    Challenge 5

--- Basic_node_&_express.js
+++ Basic_node_&_express.js
@@ -1,1 +1,24 @@
+let express = require('express');
+let app = express();
+console.log("Hello World");
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/json', function(req, res){
+  const data = {
+    "message": "Hello json"
+  };
+  res.json(data); 
+})
+
+app.get('/', (req, res) => {
+  absolutePath = __dirname + '/views/index.html'
+
+  res.sendFile(absolutePath);
+
+});
+
+
+
+module.exports = app;
 
