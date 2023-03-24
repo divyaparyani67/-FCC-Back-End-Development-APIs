c43a0b75d97b315f0e3636cc092d25b3434e7dca
Divya Paryani <106226581+divyaparyani67@users.noreply.github.com>
2023-03-25 02:09:58 +0530

    Update Basic_node_&_express.js

--- Basic_node_&_express.js
+++ Basic_node_&_express.js
@@ -4,11 +4,40 @@ console.log("Hello World");
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/json', function(req, res){
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next()
+});
+
+app.route('/name').get(function(req, res) {
+  console.log(req.query)
+  res.json({ name: `${req.query.first} ${req.query.last}` });
+})
+
+app.get('/:word/echo', function(req, res) {
+
+  res.json({ echo: req.params.word });
+});
+
+app.get('/now', function(req, res, next) {
+  req.time = (new Date()).toString();
+  next()
+}, function(req, res) {
+  res.json({ time: req.time })
+});
+
+app.get('/json', function(req, res) {
+  const helloJSON = "Hello json";
+  let message;
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = helloJSON.toUpperCase();
+  } else {
+    message = helloJSON
+  }
   const data = {
-    "message": "Hello json"
+    "message": message
   };
-  res.json(data); 
+  res.json(data);
 })
 
 app.get('/', (req, res) => {
@@ -22,3 +51,41 @@ app.get('/', (req, res) => {
 
 module.exports = app;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
